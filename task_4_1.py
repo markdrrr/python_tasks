c2b404d092c4dccfa113a3ebbcd64bbd68461d23
markdrrr <markdrrr@gmail.com>
2020-07-30 20:06:34 +0300

    Homework - 4 task_4_1 second solution

--- task_4_1.py
+++ task_4_1.py
@@ -2,10 +2,20 @@
 # Создать новый список, каждый элемент которого
 # равен исходному элементу умноженному на -2
 number_integer = [5, 6, 10, 12, 15]
+print(number_integer)
 new_list_number = []
-for i in number_integer:
+for i in number_integer: # решение №1
     new_number = i * -2
     new_list_number.append(new_number)
-
-print(number_integer)
 print(new_list_number)
+
+number_integer2 = [7, 3, 1, 2, 5]
+print(number_integer2)
+new_list_number2 = []
+i = 0  # счетчик для цикла
+while i < len(number_integer2):  # решение №2
+    new_number = number_integer2[i] * -2
+    new_list_number2.append(new_number)
+    i += 1
+print(new_list_number2)
+
